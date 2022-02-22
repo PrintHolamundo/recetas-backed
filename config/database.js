6bde95c1d3224180f87e4e59a6dde017949c568b
PrintHolamundo <jdohca@gmail.com>
2022-02-21 23:38:45 -0600

    Inicio backend recetas

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'casserole-db.o4z7j.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'myFirstDatabase'),
+        username: env('DATABASE_USERNAME', 'david_admin'),
+        password: env('DATABASE_PASSWORD', 'root123'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c7cc2541-3cd2-409e-a68e-c968e45c5fbe'
+};
